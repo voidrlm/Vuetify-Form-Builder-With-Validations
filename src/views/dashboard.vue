10f1e065af8a3afb84834f5d50f26ad9e9c11951
voidrlm <varjun609@gmail.com>
2023-01-29 12:21:27 +0530

    Added initial desing layout

--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -1,10 +1,14 @@
 <template>
-  <v-container fluid>
-    <v-card-title
-      :class="
-        $vuetify.breakpoint.mdAndUp ? 'text-h4 mt-n7' : 'text-h5 justify-center'
-      "
-      >Vuetify Form Builder</v-card-title
+  <v-container fluid fill-height>
+    <v-layout row wrap>
+      <v-flex xs12 sm12 md6>
+        <v-card-title class="justify-center h5"
+          >Playground</v-card-title
+        ></v-flex
+      ><v-divider vertical />
+      <v-flex xs12 sm12 md6>
+        <v-card-title class="justify-center h5">Code</v-card-title></v-flex
+      ></v-layout
     >
   </v-container>
 </template>
