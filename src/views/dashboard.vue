8285e95c9b1ff79a4d5f93250af5adc8a8803a4e
voidrlm <varjun609@gmail.com>
2023-01-29 12:29:29 +0530

    Declared components in dashboard

--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -2,20 +2,25 @@
   <v-container fluid fill-height>
     <v-layout row wrap>
       <v-flex xs12 sm12 md6>
-        <v-card-title class="justify-center h5"
-          >Playground</v-card-title
-        ></v-flex
+        <v-card-title class="justify-center h5">Playground</v-card-title
+        ><formBuilder /></v-flex
       ><v-divider vertical />
       <v-flex xs12 sm12 md6>
-        <v-card-title class="justify-center h5">Code</v-card-title></v-flex
-      ></v-layout
-    >
+        <v-card-title class="justify-center h5">Code</v-card-title
+        ><codeBlock /></v-flex
+    ></v-layout>
   </v-container>
 </template>
 <script>
+import formBuilder from "../components/form/formBuilder.vue";
+import codeBlock from "@/components/code/codeBlock.vue";
 export default {
   name: "dashboard-component",
   data: () => ({}),
+  components: {
+    formBuilder,
+    codeBlock,
+  },
   computed: {},
 };
 </script>
