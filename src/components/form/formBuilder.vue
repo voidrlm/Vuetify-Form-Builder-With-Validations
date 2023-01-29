b535c7d5d6e0132a868f8b51ae3f19cfde43601e
voidrlm <varjun609@gmail.com>
2023-01-29 16:57:36 +0530

    Design improvements

--- src/components/form/formBuilder.vue
+++ src/components/form/formBuilder.vue
@@ -2,19 +2,21 @@
   <div>
     <v-card
       class="accent mb-5 pa-0 mx-5 rounded-lg"
+      elevation="0"
       v-for="(textfield, index) in form"
       :key="index"
     >
       <v-card-text
         ><v-layout row wrap>
-          <v-flex xs8 sm8 md8 class="mb-n4"
+          <v-flex xs8 sm8 md8
             ><v-text-field
               rounded
               v-model="textfield.title"
               solo
               class="ma-2"
+              flat
             ></v-text-field></v-flex
-          ><v-flex xs4 sm4 md4 class="mb-n4"
+          ><v-flex xs4 sm4 md4
             ><v-select
               rounded
               v-model="textfield.type"
@@ -26,10 +28,11 @@
     ></v-card>
     <v-layout justify-end class="mr-5">
       <v-btn
+        elevation="0"
         rounded
         color="accent"
         @click.stop="$emit('addField')"
-        :class="!$vuetify.theme.dark ? 'white--text' : 'black--text'"
+        :class="$vuetify.theme.dark ? 'white--text' : 'black--text'"
       >
         <v-icon size="25px" class="mr-2">mdi-plus-circle</v-icon>Add text field
       </v-btn></v-layout
