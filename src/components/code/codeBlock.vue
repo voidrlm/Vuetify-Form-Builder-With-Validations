a567b30e94508e222dbaefd34c7bd37aa0eeb56c
voidrlm <varjun609@gmail.com>
2023-01-29 13:56:29 +0530

    Code changes in codeBlock component

--- src/components/code/codeBlock.vue
+++ src/components/code/codeBlock.vue
@@ -1,12 +1,36 @@
 <template>
-  <div></div>
+  <div>
+    <v-textarea
+      background-color="grey"
+      readonly
+      auto-grow
+      solo
+      :value="fullCode"
+      class="rounded-xl mx-5"
+    ></v-textarea>
+  </div>
 </template>
 
 <script>
+import { template, script, methods, data } from "../../resources/baseCode";
 export default {
   components: {},
   props: { code: Array },
   data: () => ({}),
+  computed: {
+    fullCode() {
+      return (
+        template[0] +
+        this.code +
+        template[1] +
+        script[0] +
+        data[0] +
+        data[1] +
+        methods +
+        script[1]
+      );
+    },
+  },
 };
 </script>
 <style></style>
