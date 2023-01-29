fa3212003bcbd0f40787a1692edf9f56966f7501
voidrlm <varjun609@gmail.com>
2023-01-29 13:56:03 +0530

    Added basic code teamplates

---
+++ src/resources/baseCode.js
@@ -0,0 +1,16 @@
+export const template = [
+  "<template> \n  <v-form lazy-validation v-model=" +
+    "Valid" +
+    " ref=" +
+    "Form" +
+    ">\n    <v-container class=" +
+    "text-center" +
+    ">",
+  "\n    </v-container>\n  </v-form>\n</template>",
+];
+
+export const script = ["\n<script>\n  export default {", "\n  }\n</script>"];
+export const data = ["\n  data: () => ({\n  valid: true,", "\n  }),"];
+export const methods = [
+  "\n  methods: {\n    validate () {\n    this.$refs.form.validate()\n    }, },",
+];
