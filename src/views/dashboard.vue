98b3fc5a07dc3ae1645a87cfaf7d15241afdd8e8
voidrlm <varjun609@gmail.com>
2023-02-04 02:57:59 +0530

    Removed footer from dashboard view

--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -13,32 +13,6 @@
         <v-card-title class="justify-center h5">Code</v-card-title
         ><code-block :code="formData" /></v-flex
     ></v-layout>
-    <v-footer color="secondary darken-1" padless app>
-      <v-row justify="center" no-gutters>
-        <v-btn
-          color="white"
-          text
-          rounded
-          class="my-2"
-          href="https://twitter.com/voidrlm"
-          target="_blank"
-        >
-          <v-icon size="24px"> mdi-twitter </v-icon>
-          Twitter
-        </v-btn>
-        <v-btn
-          color="white"
-          text
-          rounded
-          class="my-2"
-          href="https://github.com/voidrlm/Vuetify-Form-Builder-With-Validations"
-          target="_blank"
-        >
-          <v-icon size="24px"> mdi-github </v-icon>
-          Github
-        </v-btn>
-      </v-row>
-    </v-footer>
   </v-container>
 </template>
 <script>
