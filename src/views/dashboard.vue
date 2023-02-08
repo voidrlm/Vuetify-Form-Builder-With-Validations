3f83cc65f19f0caf25a2f7a1023e038bb398d05c
voidrlm <varjun609@gmail.com>
2023-02-09 00:22:35 +0530

    Initial implementation for selection of templates

--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -2,8 +2,30 @@
   <v-container fluid>
     <v-layout row wrap>
       <v-flex xs12 sm12 md6>
-        <v-card-title class="justify-center h5">Playground</v-card-title
-        ><form-builder
+        <v-card-title class="justify-center h5">Playground</v-card-title>
+        <v-card class="accent mb-5 pa-2 mx-5 rounded-lg" elevation="0">
+          <v-sheet class="mx-3 transparent" max-width="700">
+            <v-slide-group multiple show-arrows>
+              <v-slide-item
+                v-for="(item, index) in templates"
+                :key="index"
+                v-slot="{ active }"
+              >
+                <v-btn
+                  class="mx-2"
+                  :input-value="active"
+                  active-class="purple white--text"
+                  depressed
+                  rounded
+                  @click="formData = item.code"
+                >
+                  {{ item.title }}
+                </v-btn>
+              </v-slide-item>
+            </v-slide-group>
+          </v-sheet></v-card
+        >
+        <form-builder
           class="mt-3"
           @addField="addTextField()"
           @removeField="removeTextField"
@@ -18,10 +40,25 @@
 <script>
 import formBuilder from "../components/form/formBuilder.vue";
 import codeBlock from "@/components/code/codeBlock.vue";
+import {
+  signupForm,
+  signin,
+  signUp2,
+  contactUs,
+  sales,
+} from "@/resources/templateCodes";
 export default {
   name: "dashboard-component",
   data: () => ({
     formData: [],
+    model: null,
+    templates: [
+      { title: "Sign-up", code: signupForm },
+      { title: "Sign-In", code: signin },
+      { title: "Sign-up 2", code: signUp2 },
+      { title: "Contact Us", code: contactUs },
+      { title: "Sales", code: sales },
+    ],
   }),
   components: {
     formBuilder,
@@ -44,6 +81,7 @@ export default {
         minCurrentDay: false,
         max: 0,
         showPassOnField: false,
+        showDollarPrefix: false,
       });
     },
     removeTextField: function (index) {
