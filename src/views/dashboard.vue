eff243601a3290e56120ce37e61d2f94da19b7a8
voidrlm <varjun609@gmail.com>
2026-02-03 17:55:59 -0500

    Add code generation settings panel with Vuetify version, API style, and TypeScript options

--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -30,8 +30,66 @@
             ><v-divider vertical v-if="!$vuetify.breakpoint.xsOnly" />
             <v-flex xs12 sm12 md6>
                 <v-divider class="mt-5" v-if="$vuetify.breakpoint.xsOnly" />
-                <v-card-title class="justify-center h5" id="codeSection">Code</v-card-title
-                ><code-block :code="formData" /></v-flex
+                <v-card-title class="justify-center h5" id="codeSection">Code</v-card-title>
+
+                <!-- Code Generation Settings Panel -->
+                <v-card class="accent mb-4 pa-3 mx-5 rounded-lg" elevation="0">
+                    <v-row dense align="center" class="mx-1">
+                        <!-- Vuetify Version Selector -->
+                        <v-col cols="12" sm="4">
+                            <div class="caption mb-1 text-center">Vuetify Version</div>
+                            <v-btn-toggle
+                                v-model="vuetifyVersion"
+                                mandatory
+                                dense
+                                rounded
+                                class="d-flex justify-center"
+                            >
+                                <v-btn :value="2" small>V2</v-btn>
+                                <v-btn :value="3" small>V3</v-btn>
+                                <v-btn :value="4" small>V4</v-btn>
+                            </v-btn-toggle>
+                        </v-col>
+
+                        <!-- API Style Selector (only for v3/v4) -->
+                        <v-col cols="12" sm="4">
+                            <div class="caption mb-1 text-center">API Style</div>
+                            <v-btn-toggle
+                                v-model="apiStyle"
+                                mandatory
+                                dense
+                                rounded
+                                class="d-flex justify-center"
+                                :disabled="vuetifyVersion === 2"
+                            >
+                                <v-btn value="options" small :disabled="vuetifyVersion === 2">Options</v-btn>
+                                <v-btn value="composition" small :disabled="vuetifyVersion === 2">Composition</v-btn>
+                            </v-btn-toggle>
+                        </v-col>
+
+                        <!-- TypeScript Toggle -->
+                        <v-col cols="12" sm="4">
+                            <div class="caption mb-1 text-center">Language</div>
+                            <v-btn-toggle
+                                v-model="useTypeScript"
+                                mandatory
+                                dense
+                                rounded
+                                class="d-flex justify-center"
+                            >
+                                <v-btn :value="false" small>JavaScript</v-btn>
+                                <v-btn :value="true" small>TypeScript</v-btn>
+                            </v-btn-toggle>
+                        </v-col>
+                    </v-row>
+                </v-card>
+
+                <code-block
+                    :code="formData"
+                    :vuetify-version="vuetifyVersion"
+                    :api-style="apiStyle"
+                    :use-type-script="useTypeScript"
+                /></v-flex
         ></v-layout>
         <v-btn
             v-show="$vuetify.breakpoint.xsOnly"
@@ -56,6 +114,9 @@ export default {
     data: () => ({
         formData: [],
         model: null,
+        vuetifyVersion: 2,
+        apiStyle: 'options',
+        useTypeScript: false,
         templates: [
             { title: 'Blank', code: blank },
             { title: 'Sign-up', code: signupForm },
@@ -70,6 +131,14 @@ export default {
         codeBlock,
     },
     computed: {},
+    watch: {
+        vuetifyVersion(newVal) {
+            // Reset to options API if switching to Vuetify 2
+            if (newVal === 2) {
+                this.apiStyle = 'options';
+            }
+        },
+    },
     mounted() {
         this.addTextField();
     },
@@ -87,6 +156,16 @@ export default {
                 max: 0,
                 showPassOnField: false,
                 showDollarPrefix: false,
+                // New field properties
+                rows: 3,
+                autoGrow: false,
+                items: [],
+                multiple: false,
+                chips: false,
+                radioItems: [],
+                inline: false,
+                inset: false,
+                color: '',
             });
         },
         removeTextField: function (index) {
